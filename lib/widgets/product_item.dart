f32c75ba66836748a79874b96eed93be4c657b74
EngRadan <22160136@su.edu.ye>
2026-02-02 03:12:07 +0300

    ok

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -42,7 +42,16 @@ class ProductItem extends StatelessWidget {
           trailing: IconButton(
             onPressed: () {
               cart.addItem(product.id, product.price, product.title);
-              Scaffold.of(context).openDrawer();
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text(
+                    'Added item to cart!',
+                    textAlign: TextAlign.center,
+                  ),
+                  duration: Duration(seconds: 2),
+                  action: SnackBarAction(label: 'UNDO', onPressed: () {}),
+                ),
+              );
             },
             icon: Icon(Icons.shopping_cart),
             color: Theme.of(context).colorScheme.secondary,
